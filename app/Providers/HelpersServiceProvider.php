fa6051edd247a8c5b4e4464cc9fafefa86f553aa
Yuri Bondar <trigur@yandex.ru>
2018-04-03 16:27:29 +0300

    Added helpers fix

--- app/Providers/HelpersServiceProvider.php
+++ app/Providers/HelpersServiceProvider.php
@@ -23,6 +23,8 @@ class HelpersServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        foreach (glob(app_path() . '/Helpers/*.php') as $filename) {
+            require_once ($filename);
+        }
     }
 }
