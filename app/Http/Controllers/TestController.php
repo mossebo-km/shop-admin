f45d9fa5f01d9c97ff76e28e3e749a87e569dc6c
Yuri Bondar <trigur@yandex.ru>
2018-04-04 12:12:58 +0300

    time

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,7 +8,7 @@ class TestController extends Controller
 {
     public function imageConverter()
     {
-        ni_set('max_execution_time', 0); //6 minutes
+        ini_set('max_execution_time', 0); //6 minutes
         ini_set('request_terminate_timeout', 0); //6 minutes
 
         foreach (Product::all() as $product) {
