a4f2a3267cb819cc395697747d7fd44fb183d604
Yuri Bondar <trigur@yandex.ru>
2018-11-09 14:44:14 +0300

    fix

--- app/Validation/ValidatorExtend.php
+++ app/Validation/ValidatorExtend.php
@@ -84,7 +84,7 @@ class ValidatorExtend
             return true;
         }
 
-        $childrensIds = Category::rawQuery()->getDescendantIds($recordId);
+        $childrensIds = Category::getDescendantIds($recordId);
         $childrensIds[] = $recordId;
 
         return !in_array($parentId, $childrensIds);
